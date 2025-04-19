9140a974d9ffef9a1c2a5b99d36bd5911faf2276
jimjasonp <132594623+jimjasonp@users.noreply.github.com>
2025-04-19 19:10:07 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,44 +1,32 @@
-from helper_functions import X_set,y_set,fourier_nrm_vector,fourier_nrm_vector_harmonics
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler,LabelEncoder
-from sklearn.metrics import confusion_matrix,accuracy_score,ConfusionMatrixDisplay
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import LeaveOneOut,cross_val_score,KFold
-from sklearn.neighbors import KNeighborsClassifier
+from helper_functions import X_set,y_set
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,accuracy_score
+import numpy as np
+from models import svc
+from sklearn.preprocessing import StandardScaler
+path = 'Balanced_data'
 
 
-X_clf = fourier_nrm_vector('test_classification')
-X_data = fourier_nrm_vector('Balanced_data')
 
-y_clf = y_set('test_classification')['defect']
-y_data = y_set('Balanced_data')['defect']
 
-le = LabelEncoder()
-y_data = le.fit_transform(y_data)
-y_clf = le.transform(y_clf)
+X = X_set(path,'none')[0]
+y = y_set(path)['defect']
+
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,shuffle=True)
 
 
 scaler = StandardScaler()
-X_clf= scaler.fit_transform(X_clf)
-X_data = scaler.transform(X_data)
-
-X = np.concatenate((X_data,X_clf),axis=0)
-y = np.concatenate((y_data,y_clf),axis=0)
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from xgboost import XGBClassifier
+#X_test = add_noiz(X_test,0.5,0)
 
-xgb = XGBClassifier()
-svm=SVC(C=100,gamma=0.001,kernel='rbf')
-rf = RandomForestClassifier(n_estimators=500,criterion='entropy')
+y_pred = svc(X_train,y_train,X_test)
 
-model_list = [xgb,svm,rf]
-for model in model_list:
-        for i in range(0,10):
-                scores = cross_val_score(model,X,y,cv=KFold(n_splits=10,shuffle=True))
-                print(np.mean(scores),np.median(scores))
-        print(model)
+acc = accuracy_score(y_test,y_pred)
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
+print(acc)
